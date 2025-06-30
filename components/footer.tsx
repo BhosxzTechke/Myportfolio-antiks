c58c7d22318e2bfb18f965a8f1020702064ee6d4
BhosxzTechke <danmichaelantiquina9@gmail.com>
2025-06-30 13:44:12 +0800

    updatingagain

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,12 +4,14 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2030 Ricardo. All rights reserved.
+        &copy; {new Date().getFullYear()} Michael Antiquina. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
+        Next.js, TypeScript, Tailwind CSS, Framer Motion, and deployed on Vercel.
+      </p>
+      <p className="mt-1 text-xs italic">
+        Design inspired by <a href="https://github.com/ByteGrad/portfolio-website" target="_blank" rel="noopener noreferrer" className="underline">ByteGrad (GitHub)</a>.
       </p>
     </footer>
   );
